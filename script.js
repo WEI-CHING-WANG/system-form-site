077a0ba991bc3b7f222b922e42f8bb6501fcaffa
kenny5620 <k56205620@cht.com.tw>
2025-07-16 15:44:17 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
- const s3name = sessionStorage.getItem('s3name'); 
+const API_URL = 'https://c51s3uxotb.execute-api.ap-northeast-1.amazonaws.com/myDeployment/callmygraph'
+fetch(API_URL)
+     .then(response => response.json())
+    .then(data => {
+        console.log('Fetched data:', data);
+        console.log('Post title:', data.title);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+
+const s3name = sessionStorage.getItem('s3name'); 
  var currentdate = new Date(); 
  var datetime = "Last Sync: " + currentdate.getFullYear() 
                 + ("0" + (currentdate.getMonth() + 1)).slice(-2)
