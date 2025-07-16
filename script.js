fb71f7118f6af7ce322656677ec72cdfcd838e82
kenny5620 <k56205620@cht.com.tw>
2025-07-16 11:53:13 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
  const s3name = sessionStorage.getItem('s3name'); 
  var currentdate = new Date(); 
  var datetime = "Last Sync: " + currentdate.getFullYear() 
-                +"0"+ (currentdate.getMonth()+1) 
-                + currentdate.getDate()   
+                + ("0" + (currentdate.getMonth() + 1)).slice(-2)
+                + ("0" + currentdate.getDate()).slice(-2)   
                 + currentdate.getHours() 
                 + currentdate.getMinutes();
- document.getElementById('output').innerHTML = datetime;
+ document.getElementById('output').innerHTML = datetime+s3name;
