63552966ec525368b13b335a8ee361559e25b210
kenny5620 <k56205620@cht.com.tw>
2025-07-16 17:04:56 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://ookaco6lh1.execute-api.ap-northeast-1.amazonaws.com/russia/callmygraph';
+const API_URL = 'https://wbleu2ozz7.execute-api.ap-northeast-1.amazonaws.com/default/callmygraph';
 
 const response = await fetch(API_URL, {
             method: 'POST', // 指定 HTTP 方法
