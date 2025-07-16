b80a9c582a465ad7eccfe8e4e91488ab206a539c
kenny5620 <k56205620@cht.com.tw>
2025-07-16 16:18:33 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 const API_URL = 'https://c51s3uxotb.execute-api.ap-northeast-1.amazonaws.com/myDeployment/callmygraph'
-fetch(API_URL)
-     .then(response => response.json())
-    .then(data => {
-        console.log('Fetched data:', data);
-        console.log('Post title:', data.title);
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
 
+const response = await fetch(API_URL, {
+            method: 'GET', // 指定 HTTP 方法
+            headers: {
+                'Content-Type': 'application/json',
+                // 如果您需要傳遞身份驗證 token (例如 Cognito ID Token)，可以在這裡添加
+                // 'Authorization': `Bearer ${yourAuthToken}`
+            }
+        });
+
+
+        // 解析 JSON 回應
+ const data = await response.json();
+
+ console.log('Lambda 回應資料:', data);
+
+       
 const s3name = sessionStorage.getItem('s3name'); 
  var currentdate = new Date(); 
  var datetime = "Last Sync: " + currentdate.getFullYear() 
