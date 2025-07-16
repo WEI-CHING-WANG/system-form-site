6a58cada2aec26e315597da236770bdfd36176bb
kenny5620 <k56205620@cht.com.tw>
2025-07-16 16:40:45 +0800

    Update script.js

--- script.js
+++ script.js
@@ -11,16 +11,16 @@ const response = await fetch(API_URL, {
 
 
         // 解析 JSON 回應
- const data = await response.json();
+const data = await response.json();
 
  //console.log('Lambda 回應資料:', data);
 
        
 const s3name = sessionStorage.getItem('s3name'); 
- var currentdate = new Date(); 
- var datetime = "Last Sync: " + currentdate.getFullYear() 
+var currentdate = new Date(); 
+var datetime = "Last Sync: " + currentdate.getFullYear() 
                 + ("0" + (currentdate.getMonth() + 1)).slice(-2)
                 + ("0" + currentdate.getDate()).slice(-2)   
                 + currentdate.getHours() 
                 + currentdate.getMinutes();
- document.getElementById('output').innerHTML = datetime+data;
+document.getElementById('output').innerHTML = datetime+data;
